49f4f5ddcd253480568f7fb23613c70903cc5c16
vinicius <vinicius.coassis@gmail.com>
2022-10-01 18:55:11 -0300

    feat: add categories route

--- src/server.js
+++ src/server.js
@@ -7,9 +7,33 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.get("/status", async (req, res) => {
-  const clientes = await connection.query("SELECT * FROM customers;");
+app.get("/categories", async (req, res) => {
+  const clientes = await connection.query("SELECT * FROM categories;");
   res.send(clientes.rows);
+});
+
+app.post("/categories", async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.sendStatus(400);
+  }
+
+  const NameExiste = await connection.query(
+    "SELECT * FROM categories WHERE name = $1",
+    [name]
+  );
+
+  if (NameExiste.rows.length != 0) {
+    return res.status(409).send("Este jogo já existe");
+  }
+
+  const rep = await connection.query(
+    "INSERT INTO categories (name) VALUES ($1)",
+    [name]
+  );
+
+  res.sendStatus(201);
 });
 
 app.listen(4000, () => console.log("listening..."));
